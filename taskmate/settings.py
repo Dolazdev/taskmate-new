a6e8162551f87e6b0e5d4019cc24b472afa4172a
Dolazdev <tesleemmayowa@outlook.com>
2023-10-01 09:46:43 +0100

    new update

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 import environ
@@ -89,16 +90,23 @@ WSGI_APPLICATION = 'taskmate.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env("DJANGO_DB_NAME"),
+#         'USER': env("DJANGO_DB_USER"),
+#         'PASSWORD': env("DJANGO_DB_PASSWORD"),
+#         'HOST': env("DJANGO_DB_HOST"),
+#         'PORT': env("DJANGO_DB_PORT"),
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DJANGO_DB_NAME"),
-        'USER': env("DJANGO_DB_USER"),
-        'PASSWORD': env("DJANGO_DB_PASSWORD"),
-        'HOST': env("DJANGO_DB_HOST"),
-        'PORT': env("DJANGO_DB_PORT"),
-    }
+
+    'default': dj_database_url.parse(env("DATABASE_URL"))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
